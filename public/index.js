89ee03c42d0291886e50af5106caf966475d1e79
“deleylagit config --global user.email “deleyla@gmail.com <deleyla@gmail.com>
2018-02-06 21:22:55 -0600

    Removed welcome to vue messaging

--- public/index.js
+++ public/index.js
@@ -249,7 +249,7 @@ var CampLoginPage = {
           axios.defaults.headers.common["Authorization"] =
             "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
-          router.push("/");
+          router.push("/scholarship");
         })
         .catch(
           function(error) {
